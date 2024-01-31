78d0ce188bb3aadcd98c93beca1812e1e0203ea5
danangfirmino26@gmail.com <danangfirmino26@gmail.com>
2024-01-31 20:22:29 +0700

    update proses harian

--- scrapErigo/pipelines.py
+++ scrapErigo/pipelines.py
@@ -8,6 +8,6 @@
 from itemadapter import ItemAdapter
 
 
-class ScrapErigoPipeline:
+class scrapErigoPipeline:
     def process_item(self, item, spider):
         return item
--- scrapErigo/settings.py
+++ scrapErigo/settings.py
@@ -7,10 +7,10 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = "ScrapErigo"
+BOT_NAME = "scrapErigo"
 
-SPIDER_MODULES = ["ScrapErigo.spiders"]
-NEWSPIDER_MODULE = "ScrapErigo.spiders"
+SPIDER_MODULES = ["scrapErigo.spiders"]
+NEWSPIDER_MODULE = "scrapErigo.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
