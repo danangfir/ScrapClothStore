8731335edf0fc44a57adb3a7b7d8cb380f09033d
danangfirmino26@gmail.com <danangfirmino26@gmail.com>
2024-02-02 14:48:19 +0700

    burnout gwej

--- scrapErigo/spiders/spider-1.py
+++ scrapErigo/spiders/spider-1.py
@@ -19,6 +19,12 @@ class ErigoScrap(scrapy.Spider):
                 'price' : product.css ('span.price-item price-item--sale').get(),
                 'url'   : product.css ('div.card-information').attrib['href'],
             }
+            
+        next_page = response.css('[rel="next"] ::attr(href)').get()
+
+        if next_page is not None:
+            next_page_url = 'https://erigostore.co.id/' + next_page
+            yield response.follow(next_page_url, callback=self.parse)
     
     
     
