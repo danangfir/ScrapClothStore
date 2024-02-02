01e1d69a48a3a2af465506e48abf6c359b705b5f
danangfirmino26@gmail.com <danangfirmino26@gmail.com>
2024-02-02 20:20:35 +0700

    clear project (need more cleaning data)

--- scrapErigo/spiders/spider-1.py
+++ scrapErigo/spiders/spider-1.py
@@ -1,30 +1,20 @@
 import scrapy
 
 class ErigoScrap(scrapy.Spider):
-    # nama dari spider
     name = 'erigoSpider'
-    # url web yang mau di scrape
-    allowed_domains = ['https://erigostore.co.id/']
-    starts_url = ['https://erigostore.co.id/collections/all-shirt']
-    
+    allowed_domains = ['erigostore.co.id']
+    start_urls = ['https://erigostore.co.id/collections/all-shirt']
     
     def parse(self, response):
-        
-        # Looping
         products = response.css('li.product')
         for product in products:
-            # tambahkan data atau tag web
             yield {
-                'name'  : product.css ('a.card-title link-underline card-title-ellipsis').get(),
-                'price' : product.css ('span.price-item price-item--sale').get(),
-                'url'   : product.css ('div.card-information').attrib['href'],
+                'name': product.css('a.card-title::text').get(),
+                'price': product.css('span.price-item::text').get(),
+                'url': product.css('a.card-title::attr(href)').get(),  # Pastikan selector ini benar
             }
             
         next_page = response.css('[rel="next"] ::attr(href)').get()
-
         if next_page is not None:
-            next_page_url = 'https://erigostore.co.id/' + next_page
+            next_page_url = response.urljoin(next_page)
             yield response.follow(next_page_url, callback=self.parse)
-    
-    
-    
---
+++ scrapErigo/spiders/spider-2.py
@@ -0,0 +1,31 @@
+"""
+import scrapy
+
+class ErigoScrap(scrapy.Spider):
+    # nama dari spider
+    name = 'erigoSpider'
+    # url web yang mau di scrape
+    allowed_domains = ['https://erigostore.co.id/']
+    starts_url = ['https://erigostore.co.id/collections/all-shirt']
+    
+    
+    def parse(self, response):
+        
+        # Looping
+        products = response.css('li.product')
+        for product in products:
+            # tambahkan data atau tag web
+            yield {
+                'name'  : product.css ('a.card-title::text').get(),
+                'price' : product.css ('span.price-item::text').get(),
+                'url'   : product.css ('div.card-information').attrib['href'],
+            }
+            
+        next_page = response.css('[rel="next"] ::attr(href)').get()
+
+        if next_page is not None:
+            next_page_url = 'https://erigostore.co.id' + next_page
+            yield response.follow(next_page_url, callback=self.parse)
+"""
+    
+    
